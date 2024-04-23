146b5798d0a2f7d9b9f7ca80f8d31fda3ea64f69
Marcin Pluta <pluciorx@gmail.com>
2024-04-23 16:34:57 +0200

    initial commit of the source code

---
+++ WatchWinderSource/WatchWinderSource.ino
@@ -0,0 +1,51 @@
+#include <AccelStepper.h>
+
+#define IN1 D5
+#define IN2 D6
+#define IN3 D7
+#define IN4 D8
+
+AccelStepper stepper(AccelStepper::HALF4WIRE, IN1, IN3, IN2, IN4);
+
+const long stepsPerRevolution = 2048;
+
+#define ROTATIONS 15
+#define ROTATIONS_IN_ONE_DIRECTION ROTATIONS/2
+int TimeSleepS = 480;
+
+void setup()
+{
+    Serial.begin(9600);     // 9600 bps
+    Serial.println("");
+    Serial.println("pluciorx Watch Rotor Programm v1.0 ");
+    stepper.setMaxSpeed(1650);
+    stepper.setAcceleration(500);
+    stepper.moveTo(stepsPerRevolution * ROTATIONS_IN_ONE_DIRECTION);
+}
+
+void loop()
+{
+    int counter = 0;
+
+    while (counter < ROTATIONS)
+    {
+        stepper.setMinPulseWidth(15);
+
+        stepper.runToPosition();
+        counter++;
+        Serial.print("Revolution:"); Serial.println(counter);
+        if (counter >= ROTATIONS / 2)
+        {
+            stepper.move(-1 * (stepsPerRevolution * ROTATIONS_IN_ONE_DIRECTION));
+        }
+        else
+        {
+            stepper.move(stepsPerRevolution * ROTATIONS_IN_ONE_DIRECTION);
+        }
+    }
+    Serial.print("Pause for :"); Serial.print(TimeSleepS); Serial.println("s");
+    stepper.disableOutputs();
+    delay(TimeSleepS * 1000);
+    stepper.enableOutputs();
+    Serial.println("Pause finished");
+}
